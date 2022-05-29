59949838298148c294c82de3bcac002000c0b9bf
Igor Santos <falaigors@gmail.com>
2022-05-29 18:02:57 -0300

    configure cors

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,20 @@ import { router } from "./router";
 
 const app = express();
 
+app.use(express.json());
 app.use(
   cors({
-    origin: ["https://tag-memory-test.vercel.app/"],
+    origin: ["https://tag-memory-test.vercel.app"],
     optionsSuccessStatus: 200,
   })
 );
-app.use(express.json());
+
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use(router);
 
