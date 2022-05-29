4b40b9febc47c1236dd62bd63b9f046a423d7ecb
Igor Santos <falaigors@gmail.com>
2022-05-29 17:23:52 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,6 @@ import { router } from "./router";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,18 +15,6 @@ app.use((req, res, next) => {
 });
 
 app.use(router);
-
-app.get("/auth/github", (request, response) => {
-  response.redirect(
-    `https://github.com/login/oauth/authorize?scope=user&client_id=${process.env.GITHUB_API_CLIENT_ID}`
-  );
-});
-
-app.get("/auth/github/callback", (request, response) => {
-  const { code } = request.query;
-
-  return response.json(code);
-});
 
 app.listen(process.env.PORT || 3333, () =>
   console.log(`🚀 Server is runing on PORT ${process.env.PORT}`)
