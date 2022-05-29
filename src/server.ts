90b70e019ce077d0cb2493557f3470053389570a
Igor Santos <falaigors@gmail.com>
2022-05-29 17:16:42 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
+
 app.use(router);
 
 app.get("/auth/github", (request, response) => {
